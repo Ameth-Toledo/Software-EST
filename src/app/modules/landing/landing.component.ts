c1ad107d145324685dae6ed04f6bb33e8502a072
Ameth-Toledo <233363@ids.upchiapas.edu.mx>
2025-04-20 04:11:21 -0600

    tecnologias

--- src/app/modules/landing/landing.component.ts
+++ src/app/modules/landing/landing.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from "../../components/header/header.component";
 import { Router } from '@angular/router';
 import { FooterComponent } from "../../components/footer/footer.component";
 import { CardCursosComponent } from "../../components/card-cursos/card-cursos.component";
+import { TecnologiasComponent } from "../../components/tecnologias/tecnologias.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, CardCursosComponent],
+  imports: [HeaderComponent, FooterComponent, CardCursosComponent, TecnologiasComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
